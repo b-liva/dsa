8a5bbd5bf24d34e8b52d87e4e92cf5751cdd459c
liva <ebammo20@gmail.com>
2022-07-24 22:06:51 +0430

    show contact app in admin panel

--- contacts/models.py
+++ contacts/models.py
@@ -7,3 +7,7 @@ class Contact(models.Model):
     last_name = models.CharField(max_length=50)
     phone = models.IntegerField()
     job = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name
+    
